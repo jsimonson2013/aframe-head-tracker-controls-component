e0f9f2a0f5c5971fec37ca6183090855e2646911
jsimonson <jsimonson2013@gmail.com>
2017-07-25 20:59:56 -0700

    Refactored to be more A-Frame compliant. Need to do some math to figure out head movements.

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ AFRAME.registerComponent('head-tracker-controls', {
   /**
    * Called once when component is attached. Generally for initial setup.
    */
-  init: function () {
+  init: function () { },
+
+  handleHeadMovement: function(event) {
     var camera = document.querySelector('a-camera')
-    document.addEventListener('facetrackingEvent', function(event){
-      camera.setAttribute('position', 'x', 0.05*event.x)
-      camera.setAttribute('position', 'y', 0.05*event.y)
-    })
+
+    camera.setAttribute('position', 'x', -0.1*event.x)
+    camera.setAttribute('position', 'y', 1.6 + 0.1*event.y)
   },
 
   /**
@@ -47,11 +48,15 @@ AFRAME.registerComponent('head-tracker-controls', {
    * Called when entity pauses.
    * Use to stop or remove any dynamic or background behavior such as events.
    */
-  pause: function () { },
+  pause: function () {
+    document.removeEventListener('headtrackingEvent', this.handleHeadMovement)
+  },
 
   /**
    * Called when entity resumes.
    * Use to continue or add any dynamic or background behavior such as events.
    */
-  play: function () { }
+  play: function () {
+    document.addEventListener('headtrackingEvent', this.handleHeadMovement)
+  }
 });
